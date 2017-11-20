be3b2d6a8a0d06aeff1564938bf2e2bd8c93cbd2
litong.pan <litong.pan@foresight-robotics.com>
2017-11-20 17:32:19 +0800

    add eslint

--- gulpfile.js
+++ gulpfile.js
@@ -8,65 +8,71 @@ var app = {
   prdPath: 'dist/'
 };
 
-gulp.task('lib', function() {
+gulp.task('lib', function () {
   gulp.src('bower_components/**/*.js')
-  .pipe(gulp.dest(app.devPath + 'vendor'))
-  .pipe(gulp.dest(app.prdPath + 'vendor'))
-  .pipe($.connect.reload());
+    .pipe(gulp.dest(app.devPath + 'vendor'))
+    .pipe(gulp.dest(app.prdPath + 'vendor'))
+    .pipe($.connect.reload());
 });
 
-gulp.task('html', function() {
+gulp.task('html', function () {
   gulp.src(app.srcPath + '**/*.html')
-  .pipe(gulp.dest(app.devPath))
-  .pipe(gulp.dest(app.prdPath))
-  .pipe($.connect.reload());
-})
+    .pipe(gulp.dest(app.devPath))
+    .pipe(gulp.dest(app.prdPath))
+    .pipe($.connect.reload());
+});
 
-
-gulp.task('json', function() {
+gulp.task('json', function () {
   gulp.src(app.srcPath + 'data/**/*.json')
-  .pipe(gulp.dest(app.devPath + 'data'))
-  .pipe(gulp.dest(app.prdPath + 'data'))
-  .pipe($.connect.reload());
+    .pipe(gulp.dest(app.devPath + 'data'))
+    .pipe(gulp.dest(app.prdPath + 'data'))
+    .pipe($.connect.reload());
 });
 
-gulp.task('less', function() {
+gulp.task('less', function () {
   gulp.src(app.srcPath + 'style/index.less')
-  .pipe($.plumber())
-  .pipe($.less())
-  .pipe(gulp.dest(app.devPath + 'css'))
-  .pipe($.cssmin())
-  .pipe(gulp.dest(app.prdPath + 'css'))
-  .pipe($.connect.reload());
+    .pipe($.plumber())
+    .pipe($.less())
+    .pipe(gulp.dest(app.devPath + 'css'))
+    .pipe($.cssmin())
+    .pipe(gulp.dest(app.prdPath + 'css'))
+    .pipe($.connect.reload());
 });
 
-gulp.task('js', function() {
+gulp.task('js', function () {
   gulp.src(app.srcPath + 'script/**/*.js')
-  .pipe($.plumber())
-  .pipe($.concat('index.js'))
-  .pipe(gulp.dest(app.devPath + 'js'))
-  .pipe($.uglify())
-  .pipe(gulp.dest(app.prdPath + 'js'))
-  .pipe($.connect.reload());
+    .pipe($.plumber())
+    .pipe($.concat('index.js'))
+    .pipe(gulp.dest(app.devPath + 'js'))
+    .pipe($.uglify())
+    .pipe(gulp.dest(app.prdPath + 'js'))
+    .pipe($.connect.reload());
 });
 
-gulp.task('image', function() {
+gulp.task('image', function () {
   gulp.src(app.srcPath + 'image/**/*')
-  .pipe($.plumber())
-  .pipe(gulp.dest(app.devPath + 'image'))
-  .pipe($.imagemin())
-  .pipe(gulp.dest(app.prdPath + 'image'))
-  .pipe($.connect.reload());
+    .pipe($.plumber())
+    .pipe(gulp.dest(app.devPath + 'image'))
+    .pipe($.imagemin())
+    .pipe(gulp.dest(app.prdPath + 'image'))
+    .pipe($.connect.reload());
 });
 
 gulp.task('build', ['image', 'js', 'less', 'lib', 'html', 'json']);
 
-gulp.task('clean', function() {
+gulp.task('clean', function () {
   gulp.src([app.devPath, app.prdPath])
-  .pipe($.clean());
+    .pipe($.clean());
 });
 
-gulp.task('serve', ['build'], function() {
+gulp.task('lint', function () {
+  gulp.src([app.srcPath + '**/*.js', '!node_modules/**', '!bower_components/**'])
+    .pipe($.eslint())
+    .pipe($.eslint.format())
+    .pipe($.eslint.failAfterError());
+});
+
+gulp.task('serve', ['build'], function () {
   $.connect.server({
     root: [app.devPath],
     livereload: true,
@@ -83,4 +89,4 @@ gulp.task('serve', ['build'], function() {
   gulp.watch(app.srcPath + 'image/**/*', ['image']);
 });
 
-gulp.task('default', ['serve']);
+gulp.task('default', ['lint', 'serve']);
--- src/script/config/dict.js
+++ src/script/config/dict.js
@@ -1,12 +1,12 @@
 'use strict';
-angular.module('app').value('dict', {}).run(['dict', '$http', function(dict, $http){
-  $http.get('data/city.json').success(function(resp){
+angular.module('app').value('dict', {}).run(['dict', '$http', function (dict, $http) {
+  $http.get('data/city.json').success(function (resp) {
     dict.city = resp;
   });
-  $http.get('data/salary.json').success(function(resp){
+  $http.get('data/salary.json').success(function (resp) {
     dict.salary = resp;
   });
-  $http.get('data/scale.json').success(function(resp){
+  $http.get('data/scale.json').success(function (resp) {
     dict.scale = resp;
   });
 }]);
